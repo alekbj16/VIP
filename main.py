bff7225d09d165571aef1f7ad6f6f76270319d7b
aleksbj96@gmail.com <aleksbj96@gmail.com>
2022-01-05 14:13:48 +0100

    can you check this anders

--- main.py
+++ main.py
@@ -11,25 +11,28 @@ def retreive_imgs(n_categories):
     """
     categories = []
     images = []
-    file_names = []
+    f_names = []
 
     cwd = os.getcwd()
     path_to_categories = os.path.join(cwd,"101_ObjectCategories") #Path to all categories
     print(path_to_categories)
     list_of_categories = os.listdir(path_to_categories)
     for i in range(n_categories):
+        img_in_cat = []
+        f_names_cat = []
         path_to_category = os.path.join(path_to_categories,list_of_categories[i])
         images_in_category = os.listdir(path_to_category)
         categories.append(str(list_of_categories[i]))
         for img in images_in_category:
             img_path = os.path.join(path_to_category,img)
-            print(f"img_path{img_path}")
             img = cv2.imread(img_path)
             grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            cv2.imshow("image",grayscale)
-            cv2.waitKey(5000)
-            images.append(img)
+            img_in_cat.append(grayscale)
+            f_names_cat.append(img)
+        images.append(img_in_cat)
+        f_names.append(f_names_cat)
+    
+    return categories, images, f_names
 
-    print(images)
 if __name__ == "__main__":
     retreive_imgs(1)
